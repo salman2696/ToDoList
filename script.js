7c536ce6da500ac88ef1e271955fa22b8ad80c0c
Mohammad Salman <mohammad.salman2696@gmail.com>
2024-07-08 11:57:32 +0530

    js changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var btnClick = document.getElementById("button-addon2");
 var list = document.getElementById("todo-list");
 var input = document.getElementById("text");
+var date = document.getElementById('date')
 
 btnClick.addEventListener("click",  addListItem)
 
@@ -14,19 +15,25 @@ document.body.addEventListener("keydown", function (e) {
 function addListItem() {
   const newElement = document.createElement("li");
   const newIcon = document.createElement("i");
+  const newDate = document.createElement("p");
   const newList = document.createTextNode((list.childElementCount +1)+ '. ' + input.value);
-  if (input.value.trim() === "") {
-    alert("Enter valid List");
+  if (input.value.trim() === "" || date.value.trim() === "") {
+    alert("Enter valid List & Date");
   } else {
     newElement.appendChild(newList);
+    newDate.textContent = date.value;
     newElement.classList = "list-item";
+    
     
     newIcon.classList = "fa-solid fa-trash";
     newIcon.addEventListener("click", () => {
       list.removeChild(newElement);
     });
+    
+    newElement.appendChild(newDate);
     newElement.appendChild(newIcon);
     list.appendChild(newElement);
     input.value = "";
+    date.value = "";
   }
 }
